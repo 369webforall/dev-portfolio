b2820f643d1eed791843e1718f72a2d134c6db89
369webforall <369webforall@gmail.com>
2025-07-09 14:43:05 +0545

    home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <section className="space-y-4">
+      <h1 className="text-4xl font-bold">Hi, I&#39;m Brham Dev Mahato</h1>
+      <p className="text-lg">
+        A Fullstack Web Developer with a passion for building modern and
+        scalable applications.
+      </p>
+      <p className="text-md">
+        Tech Stack: HTML, CSS, JavaScript, MERN, Next.js, Tailwind CSS, ShadCN,
+        PostgreSQL, DSA, DevOps
+      </p>
+    </section>
+  );
+}
